a840cc432018e1a2e24089d67ac1ed8004af6106
Charlie <cqgator@ufl.edu>
2017-10-31 13:28:09 -0400

    actually resolved conflicts this time

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -6,14 +6,3 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     $scope.authentication = Authentication;
   }
 ]);
-<<<<<<< HEAD
-=======
-
-// var toggleVisibility = function(element) {
-//   if(element.style.display==='block'){
-//     element.style.display ='none';
-//   } else {
-//     element.style.display ='block';
-//   }
-//};
->>>>>>> timeCompareDev
